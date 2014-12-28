5a18b60021d1b0690e96e7d15d3c14f00d14169e
Julien Drolet-Noel <eurojulien@gmail.com>
2014-12-27 19:27:03 -0500

    First commit

---
+++ DBGen/Controllers/RequestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using DBGen.Models;
+
+namespace DBGen.Controllers
+{
+    public class RequestController : ApiController
+    {
+        Request[] requests = new Request[]
+        {
+            new Request{}
+        };
+
+        public IEnumerable<Request> GetAllRequests()
+        {
+            return requests;
+        }
+
+        public IHttpActionResult getRequest(int id)
+        {
+            var request = requests.FirstOrDefault((p) => p.id == id);
+            if(request == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(request);
+        }
+    }
+}
---
+++ DBGen/Models/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBGen.Models
+{
+    public class Request
+    {
+        public int id           { get; set; }
+        public String protocol  { get; set; }
+        public String port      { get; set; }
+        public String ipAddress { get; set; }
+        public String method    { get; set; }
+        public String body      { get; set; }
+        public String header    { get; set; }
+        public DateTime dateTime{ get; set; }
+    }
+}
